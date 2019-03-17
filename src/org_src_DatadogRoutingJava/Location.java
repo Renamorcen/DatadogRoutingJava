97b5dda4aa8678870fcb45a8aae63aa2d1c68511
renamorcen <renamorcen@gmail.com>
2019-03-17 18:57:43 +0000

    Linked the data in appropriate objects

--- src/org_src_DatadogRoutingJava/Location.java
+++ src/org_src_DatadogRoutingJava/Location.java
@@ -3,10 +3,48 @@
  */
 package org_src_DatadogRoutingJava;
 
+/**
+ * 
+ * Class representing each brewery with it's location and beers.
+ * @author Vytenis Sliogeris
+ *
+ */
 public class Location {
 	
-	int id;
-	String name;
-	String[] beers;
+	private String name;
+	private String[] beers;
+	private double lon;
+	private double lat;
+	
+	/**
+	 * Constructor for the breweries file.
+	 * @param id
+	 * @param name
+	 */
+	public Location(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * Method for adding a beer.
+	 * @param beername
+	 */
+	public void addBeer(String beerName) {
+		String[] beersNew = new String[1+beers.length];
+		for(int i = 0; i < beers.length; i++) {
+			beersNew[i] = beers[i];
+		}
+		beersNew[beersNew.length-1] = beerName;
+		beers = beersNew;
+	}
+	/**
+	 * Mutator for the geocodes file
+	 * @param lat
+	 * @param lon
+	 */
+	public void setLatLong(double lat, double lon) {
+		this.lat = lat;
+		this.lon = lon;
+	}
 	
 }
--- src/org_src_DatadogRoutingJava/RoutingMain.java
+++ src/org_src_DatadogRoutingJava/RoutingMain.java
@@ -6,6 +6,7 @@ package org_src_DatadogRoutingJava;
 import java.io.BufferedReader;
 import java.io.FileReader;
 import java.io.IOException;
+import java.util.HashMap;
 /**
  * 
  * Main class for the program
@@ -31,11 +32,33 @@ public class RoutingMain {
 			// TODO Auto-generated catch block <3
 			e.printStackTrace();
 		}
+		/**
+		 * Used a hashmap to avoid quadratic times for assigning latitudes and longitudes to the breweries
+		 * and to also avoid empty entries.
+		 */
+		HashMap<Integer, Location> locations = new HashMap<Integer, Location>();
+		for(int i = 0; i < breweries.length; i++) {
+			int id = Integer.parseInt(breweries[i][0]);
+			String name = breweries[i][1];
+			locations.put(id, new Location(name));
+		}
 		
+		/**
+		 * This function would perform in quadratic time if I used arbitrary ID's
+		 */
+		for(int i = 0; i < geocodes.length; i++) {
+			int id = Integer.parseInt(geocodes[i][1]);
+			double lat = Double.parseDouble(geocodes[i][2]);
+			double lon = Double.parseDouble(geocodes[i][3]);
+			locations.get(id).setLatLong(lat, lon);
+		}
 		
-		
+		for(int i = 0; i < beers.length; i++) {
+			String beerName = beers[i][2];
+			int id = Integer.parseInt(beers[i][1]);
+			locations.get(id).addBeer(beerName);
+		}
 		
 		System.out.println("Terminating!");
 	}
-
 }
